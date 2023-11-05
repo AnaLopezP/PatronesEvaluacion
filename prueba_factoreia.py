caff95acc2c68f67a75d1d95b8c2128eb2c011c8
Ana López <anamuyamiga@gmail.com>
2023-11-05 13:18:31 +0100

    cambios

--- prueba_factoreia.py
+++ prueba_factoreia.py
@@ -11,85 +11,86 @@ class Analisis_datos_factory(ABC):
         pass
 
 #clase de una factoria concreta
-class Grafica(Analisis_datos_factory):
+class Factoria_concreta1(Analisis_datos_factory):
     @abstractmethod
-    def crear_grafica_1(self):
-        pass
+    def crear_grafica(self):
+        return grafica1()
     
     @abstractmethod
-    def crear_grafica_2(self):
-        pass
+    def crear_analisis(self):
+        return analisis1()
     
 #clase de una factoria concreta
-class Analisis(Analisis_datos_factory):
+class Factoria_concreta2(Analisis_datos_factory):
     @abstractmethod
-    def crear_analisis_1(self):
-        pass
+    def crear_grafica(self):
+        return grafica2()
     
     @abstractmethod
-    def crear_analisis_2(self):
-        pass
-    
-#clase de un producto abstracto grtafica 1
-class Grafica_1(ABC):
-    @abstractmethod
-    def mostrar(self):
-        pass
+    def crear_analisis(self):
+        return analisis2
     
     
-#clase de un producto abstracto grtafica 2
-class Grafica_2(ABC):
+#clase de un producto abstracto grafica 1
+class Grafica(ABC):
     @abstractmethod
     def mostrar(self):
         pass
+    
+    
+'''#clase de un producto abstracto grtafica 2
+class Grafica_historiograma(ABC):
+    @abstractmethod
+    def mostrar(self):
+        pass'''
 
 #clase de un producto abstracto analisis 1
-class Analisis_1(ABC):
+class Analisis(ABC):
     @abstractmethod
     def mostrar(self):
         pass
-
+'''
 #clase de un producto abstracto analisis 2
 class Analisis_2(ABC):
     @abstractmethod
     def mostrar(self):
         pass
-    
+    '''
 
 #clase de un producto concreto grafica 1
-class grafica1_concreta1(Grafica_1):
+class grafica_debarras(Grafica):
     def mostrar(self):
-        print("Grafica 1 tipo 1")
+        print("Grafica barras")
         
 #clase de un producto concreto grafica 1
-class grafica1_concreta2(Grafica_1):
+class grafica_histograma(Grafica):
     def mostrar(self):
-        print("Grafica 1 tipo 2")
+        print("Grafica histograma")
         
-#clase de un producto concreto grtafica 2
-class grafica2_concreta1(Grafica_2):
+'''#clase de un producto concreto grtafica 2
+class grafica2_concreta1(Grafica_historiograma):
     def mostrar(self):
-        print("Grafica 2 tipo 1")
+        print("Grafica historiograma tipo 1")
         
-class grafica2_concreta2(Grafica_2):
+class grafica2_concreta2(Grafica_historiograma):
     def mostrar(self):
         print("Grafica 2 tipo 2")
-        
+        '''
 #clase de un producto concreto analisis 1
-class analisis1_concreta1(Analisis_1):
+class analisis_media(Analisis):
     def mostrar(self):
-        print("Analisis 1 tipo 1")
-class analisis1_concreta2(Analisis_1):
-    def mostrar(self):
-        print("Analisis 1 tipo 2")
-              
-#clase de un producto concreto analisis 2
-class analisis2_concreta1(Analisis_2):
-    def mostrar(self):
-        print("Analisis 2")
+        print("Analisis media")
         
-class analisis2_concreta2(Analisis_2):
+class analisis1_mediana(Analisis):
     def mostrar(self):
-        print("Analisis 2")
+        print("Analisis mediana")
+              
+class analisis_moda(Analisis):
+    def mostrar(self):
+        print("Analisis moda")
+        
+'''class analisis2_concreta2(Analisis_2):
+    def mostrar(self):
+        print("Analisis 2")'''
         
         
