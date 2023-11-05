07dddddc5cbfcfb017238eb1c693a22ad9ddfe3c
Ana López <anamuyamiga@gmail.com>
2023-11-05 11:39:57 +0100

    cositas

--- prueba_factoreia.py
+++ prueba_factoreia.py
@@ -8,3 +8,21 @@ class Analisis_datos_factory(ABC):
     @abstractmethod
     def crear_analisis(self):
         pass
+    
+class Grafica(Analisis_datos_factory):
+    @abstractmethod
+    def crear_grafica_1(self):
+        pass
+    
+    @abstractmethod
+    def crear_grafica_2(self):
+        pass
+    
+class Analisis(Analisis_datos_factory):
+    @abstractmethod
+    def crear_analisis_1(self):
+        pass
+    
+    @abstractmethod
+    def crear_analisis_2(self):
+        pass
