1968ce1ba0574b3f5491a88e41ab58c95458039f
Ana López <anamuyamiga@gmail.com>
2023-11-05 17:24:00 +0100

    comienzo

---
+++ Pizzeria_builder/prueba_pizaa.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+class PizzaBuilder(ABC):
+    
+    @property
+    @abstractmethod
+    def crear_pizza(self):
+        pass
+    
+    @abstractmethod
+    def crear_masa(self):
+        pass
+    
+    @abstractmethod
+    def crear_salsa(self):
+        pass
+    
+    @abstractmethod
+    def crear_ingrediente(self):
+        pass
+    
+    @abstractmethod
+    def crear_tamano(self):
+        pass
+    
+    @abstractmethod
+    def crear_tecnica(self):
+        pass
+    
+    @abstractmethod
+    def crear_presentacion(self):
+        pass
+    
+    @abstractmethod
+    def crear_extras(self):
+        pass
+    
+    @abstractmethod
+    def crear_bebidas(self):
+        pass
+    
