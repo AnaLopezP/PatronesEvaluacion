404af9d431c4f6f99aa1d018b892192ef6ba1636
Ana López <anamuyamiga@gmail.com>
2023-11-05 13:25:07 +0100

    eng

--- prueba_factoreia.py
+++ prueba_factoreia.py
@@ -99,3 +99,12 @@ def cliente(factory: Analisis_datos_factory):
     analisis = factory.crear_analisis()
     grafica.mostrar()
     analisis.mostrar()
+    
+
+if __name__ == "__main__":
+    print("Prueba de la factoria 1")
+    cliente(Factoria_concreta1())
+    print("\n")
+    print("Prueba de la factoria 2")
+    cliente(Factoria_concreta2())
+    
