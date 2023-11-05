e13278678ff293f24870f4eb2541f1ef3372442a
Ana López <anamuyamiga@gmail.com>
2023-11-05 20:41:12 +0100

    ana la rana sabe cosas

--- Pizzeria_builder/prueba_pizaa.py
+++ Pizzeria_builder/prueba_pizaa.py
@@ -136,6 +136,7 @@ if __name__ == "__main__":
     print("Creando constructor de pizza...")
     builder = Pizza()
     print("Trayendo al camarero...")
+    print("¡Benvenido a la Pizzeria AnaLaRana! Por favor, siga los pasos para crear su pizza. Tenemos TODO lo que se pueda imaginar:")
     director = PizzaDirector(builder)
     director.crear_pizza()
     pizza = builder.pizza
