6899d10932256d7906468e451f7d89503d0aa36d
Ana López <anamuyamiga@gmail.com>
2023-11-05 14:58:51 +0100

    comentarios

--- Abstract_Factory/cliente_factoria.py
+++ Abstract_Factory/cliente_factoria.py
@@ -1,5 +1,6 @@
 from factoria import *
 
+#funcion cliente que recibe una factoria
 def cliente(factory: Analisis_datos_factory):
     grafica = factory.crear_grafica()
     analisis = factory.crear_analisis()
