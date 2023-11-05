c66f57aefde849ff1ae7705b31781e58a3be5016
Ana López <anamuyamiga@gmail.com>
2023-11-05 20:46:04 +0100

    kjjk

--- Pizzeria_builder/pizzeria_AnaLaRana.py
+++ Pizzeria_builder/pizzeria_AnaLaRana.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 import csv
 import os.path
-from main import pizza
 
 class PizzaBuilder(ABC):
     
@@ -132,4 +131,19 @@ class PizzaDirector:
     @builder.setter
     def builder(self, builder):
         self._builder = builder
-        
+
+if __name__ == "__main__":
+    print("Creando constructor de pizza...")
+    builder = Pizza()
+    print("Trayendo al camarero...")
+    print("¡Benvenido a la Pizzeria AnaLaRana! Por favor, siga los pasos para crear su pizza. Tenemos TODO lo que se pueda imaginar:")
+    director = PizzaDirector(builder)
+    director.crear_pizza()
+    pizza = builder.pizza
+    print(pizza)
+    
+    
+    if not os.path.isfile('pizza.csv'):
+        csv_builder = CSV_Builder()
+        csv_builder.crear_csv()
+    csv_builder.añadir_pizza()
