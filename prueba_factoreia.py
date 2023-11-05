996527ba5b88eeba55e02825bd7f0d2124c5b059
Ana López <anamuyamiga@gmail.com>
2023-11-05 11:50:35 +0100

    ya no se que estoy haciendo :)

--- prueba_factoreia.py
+++ prueba_factoreia.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+#clase de la abstract factory
 class Analisis_datos_factory(ABC):
     @abstractmethod
     def crear_grafica(self):
@@ -8,7 +9,8 @@ class Analisis_datos_factory(ABC):
     @abstractmethod
     def crear_analisis(self):
         pass
-    
+
+#clase de una factoria concreta
 class Grafica(Analisis_datos_factory):
     @abstractmethod
     def crear_grafica_1(self):
@@ -18,6 +20,7 @@ class Grafica(Analisis_datos_factory):
     def crear_grafica_2(self):
         pass
     
+#clase de una factoria concreta
 class Analisis(Analisis_datos_factory):
     @abstractmethod
     def crear_analisis_1(self):
@@ -25,4 +28,15 @@ class Analisis(Analisis_datos_factory):
     
     @abstractmethod
     def crear_analisis_2(self):
+        pass
+    
+#clase de un producto abstracto grtafica 1
+class Grafica_1(ABC):
+    @abstractmethod
+    def mostrar(self):
+        pass
+    
+class Grafica_2(ABC):
+    @abstractmethod
+    def mostrar(self):
         pass
